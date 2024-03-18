139fd05077c66f5c76a35e7c1d4da1a2d16d4d84
whiit <ncqueescribir@gmail.com>
2024-03-17 20:13:48 -0600

    Add @astrojs/node dependency and configure adapter for standalone mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
-
-
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +10,7 @@ export default defineConfig({
   output: 'server',
   site: "https://xouleater.github.io",
   base: '/GuiaTEC',
+  adapter: node({
+    mode: "standalone"
+  })
 });
