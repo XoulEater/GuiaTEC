180191052837e360bb3a0d90fbe9e31ba40f6790
whiit <ncqueescribri@gmail.com>
2024-04-06 14:27:25 -0600

    Update user type and registration page layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,19 @@
 import { Avatar, Dropdown, Navbar } from "flowbite-react";
 import { NavBarTheme } from "../lib/themes";
 
-
 interface Props {
   userType: "asistente" | "profesor";
   currentRoute: "equipo" | "estudiantes" | "profesores";
 }
 
 const Header: React.FC<Props> = ({ currentRoute }) => {
-  const userType = localStorage.getItem("userType");
+  const userType = localStorage.getItem("user");
 
-
+  function handleLogout() {
+    console.log("Logging out...");
+    localStorage.removeItem("user");
+    window.location.href = "/";
+  }
 
   return (
     <Navbar fluid rounded className="bg-slate-50" theme={NavBarTheme}>
@@ -28,13 +31,7 @@ const Header: React.FC<Props> = ({ currentRoute }) => {
         <Dropdown
           arrowIcon={false}
           inline
-          label={
-            <Avatar
-              alt="User settings"
-              img="/userDefault.png"
-              rounded
-            />
-          }
+          label={<Avatar alt="User settings" img="/userDefault.png" rounded />}
         >
           <Dropdown.Header>
             <span className="block text-sm">Bonnie Green</span>
@@ -44,31 +41,24 @@ const Header: React.FC<Props> = ({ currentRoute }) => {
           </Dropdown.Header>
           <Dropdown.Item
             className={userType == "asistente" ? "grid" : "hidden"}
+            href="/register"
+            target="_blank"
           >
             Registrar profesor
           </Dropdown.Item>
           <Dropdown.Divider />
-          <Dropdown.Item href="/">Salir</Dropdown.Item>
+          <Dropdown.Item onClick={handleLogout}>Salir</Dropdown.Item>
         </Dropdown>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link
-          href="/team"
-          active={currentRoute === "equipo"}
-        >
+        <Navbar.Link href="/team" active={currentRoute === "equipo"}>
           Equipo
         </Navbar.Link>
-        <Navbar.Link
-          href="/students"
-          active={currentRoute === "estudiantes"}
-        >
+        <Navbar.Link href="/students" active={currentRoute === "estudiantes"}>
           Estudiantes
         </Navbar.Link>
-        <Navbar.Link
-          href="/teachers"
-          active={currentRoute === "profesores"}
-        >
+        <Navbar.Link href="/teachers" active={currentRoute === "profesores"}>
           Profesores
         </Navbar.Link>
       </Navbar.Collapse>
--- src/components/TeachersTable.tsx
+++ src/components/TeachersTable.tsx
@@ -86,10 +86,11 @@ const MembersTable = () => {
                   strokeLinejoin="round"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <path d="M13 12v.01"></path>
-                  <path d="M3 21h18"></path>
-                  <path d="M5 21v-16a2 2 0 0 1 2 -2h7.5m2.5 10.5v7.5"></path>
-                  <path d="M14 7h7m-3 -3l3 3l-3 3"></path>
+                  <path d="M4 7l16 0"></path>
+                  <path d="M10 11l0 6"></path>
+                  <path d="M14 11l0 6"></path>
+                  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
                 </svg>
               </button>
               {teacher !==
